c1f56025e3f72d0cf01b96829175ea796826d996
Liam Watts <liam.watts271@gmail.com>
2022-08-14 17:21:05 +1000

    Add caching to the query engine

--- src/analyse_conf/author_info.py
+++ src/analyse_conf/author_info.py
@@ -1,28 +1,49 @@
 """Provides functions that extract further data of each author from google scholar"""
-from typing import Optional, Any
+import os
+import pickle
 import requests
+from typing import Optional, Any
 
 from analyse_conf.data import Authorship, Author
 
-# Cache the results for each query, using a key value pair of Author name to Author data (actual value needs to be a list of authors with the same name)
-    # Then look in the cache first before making a ScraperAPI request (to save money)
-    # Persist the cache to disk at end of program 
-    # (maybe it should be a global object created by this file, that has a destructor which persists the cache to disk, and a constructor that reads from it)
-class SemanticScholarQuerier:
-    """Make queries to the google scholar API, while keeping a persisted cache of previous queries, to avoid duplicate queries across sessions"""
-    def __init__(self, api_path="https://api.semanticscholar.org/graph/v1"):
-        """Read query cache in constructor"""
-        self.api_path = api_path
 
-    # Write cache in destructor
+class SemanticScholarQuerier:
+    """
+    Make queries to the google scholar API, while keeping a persisted cache of previous queries, to avoid duplicate queries across sessions
+    Should ALWAYS be used with the WITH keyword (to load and write the cache)
+    """
+    
+    def __init__(self, api_path="https://api.semanticscholar.org/graph/v1", cache_path=".api_cache"):
+        self.__api_path = api_path
+        self.__cache_path = cache_path
+        self.__cache: dict[str, dict[str, Any]] = {} # maps API urls to Json responses
+
+    def __enter__(self) -> 'SemanticScholarQuerier':
+        """Load the cache from file"""
+        if os.path.exists(self.__cache_path):
+            with open(self.__cache_path, "rb") as file:
+                self.__cache = pickle.load(file)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        """Write the cache to file"""
+        with open(self.__cache_path, "wb") as file:
+            pickle.dump(self.__cache, file)
 
     def __get_json(self, resource_url: str) -> dict[str, Any]:
-        """Return the json for a get request on the given resource url for the SemanticScholar graph API"""
-        response = requests.get(f"{self.api_path}/{resource_url}")
+        """
+        Return the json for a get request on the given resource url for the SemanticScholar graph API
+        Cache new requests, and return the cached result for any previously seen API requests
+        """
+        if resource_url in self.__cache:
+            return self.__cache[resource_url]
+        
+        response = requests.get(f"{self.__api_path}/{resource_url}")
         response.raise_for_status()
-        return response.json()
+        json = response.json()
+        self.__cache[resource_url] = json
+        return json
 
-    # TODO: check if query is in cache
     def get_paper(self, title: str) -> Optional[dict[str, Any]]:
         """Return the paper json, if it exists"""
         paper_json = self.__get_json(f"paper/search?query={title}&fields=authors")
@@ -33,43 +54,35 @@ class SemanticScholarQuerier:
         return self.__get_json(f"author/{id}?fields=affiliations,paperCount,citationCount,hIndex")
 
 
-
-def extract_author_data(authorships: list[Authorship]) -> list[Author]:
+def get_author_data(authorships: list[Authorship]) -> list[Author]:
     """Create authors and extract their data from google scholar"""
     authors: set[Author] = set()
-    query_engine = SemanticScholarQuerier()
-
-    # Retrieve data from semantic scholar for each author
-    for authorship in authorships:
-        # only add new authors
-        if Author(authorship.author_name) in authors:
-            continue
-        
-        # Retrieve paper, if it exists
-        paper = query_engine.get_paper(authorship.title)
-        if paper is None:
-            continue
-
-        # Retrieve and add author details for the entire paper
-        for author_id_json in paper["authors"]:
-            author = Author(author_id_json["name"])
-            if author in authors:
+    with SemanticScholarQuerier() as query_engine:
+        # Retrieve data from semantic scholar for each author
+        for authorship in authorships:
+            # only add new authors
+            if Author(authorship.author_name) in authors:
+                continue
+            
+            # Retrieve paper, if it exists
+            paper = query_engine.get_paper(authorship.title)
+            if paper is None:
                 continue
 
-            # query API and fill in the author object
-            author_json = query_engine.get_author(author_id_json["authorId"])
-            author.citations = author_json["citationCount"]
-            author.paper_count = author_json["paperCount"]
-            author.h_index = author_json["hIndex"]
-            if author_json["affiliations"]:
-                author.institution = author_json["affiliations"][0]
-            
-            authors.add(author)
-            print(author)
+            # Retrieve and add author details for the entire paper
+            for author_id_json in paper["authors"]:
+                author = Author(author_id_json["name"])
+                if author in authors:
+                    continue
+
+                # query API and fill in the author object
+                author_json = query_engine.get_author(author_id_json["authorId"])
+                author.citations = author_json["citationCount"]
+                author.paper_count = author_json["paperCount"]
+                author.h_index = author_json["hIndex"]
+                if author_json["affiliations"]:
+                    author.institution = author_json["affiliations"][0]
+                
+                authors.add(author)
+                print(author)
     return list(authors)
-
-
-def get_author_data(authorships: list[Authorship]) -> list[Author]:
-    """Extract all author data, create Author objects to represent them"""
-    authors = extract_author_data(authorships) # store authors in a class, then add in extra info from google scholar
-    return authors
--- tests/author_info_tests.py
+++ tests/author_info_tests.py
@@ -1,29 +1,34 @@
 """Tests author_info.py"""
+import os
+import pickle
+import pytest
+
 from analyse_conf import sigir_extract
 from analyse_conf.author_info import SemanticScholarQuerier, get_author_data
 
 
+@pytest.mark.skip # skip for now until we've developed caching ability
 def test_get_paper() -> None:
     """Test that a subset of papers from SIGIR can be found using SemanticScholarQuerier.get_paper"""
     papers, _ = sigir_extract.extract_data()
-    query_engine = SemanticScholarQuerier()
-    for paper in papers[:10]:
-        paper_json = query_engine.get_paper(paper.title)
-        assert paper_json is not None, f"No paper found for {paper.title=}"
-        assert "authors" in paper_json, f"Authors field isn't returned for {paper.title=}"
-        assert len(paper_json["authors"]) >= 1, f"There are no authors for a paper for {paper.title=}"
+    with SemanticScholarQuerier() as query_engine:
+        for paper in papers[:10]: #TODO: run on all papers for complete testing
+            paper_json = query_engine.get_paper(paper.title)
+            assert paper_json is not None, f"No paper found for {paper.title=}"
+            assert "authors" in paper_json, f"Authors field isn't returned for {paper.title=}"
+            assert len(paper_json["authors"]) >= 1, f"There are no authors for a paper for {paper.title=}"
 
 
 def test_get_author() -> None:
     """Test that SemanticScholarQuerier.get_author returns all the required fields"""
-    query_engine = SemanticScholarQuerier()
-    author_json = query_engine.get_author("1741101")
-    assert "authorId" in author_json
-    assert "citationCount" in author_json
-    assert "paperCount" in author_json
-    assert "hIndex" in author_json
-    assert "affiliations" in author_json
-    assert isinstance(author_json["affiliations"], list)
+    with SemanticScholarQuerier() as query_engine:
+        author_json = query_engine.get_author("1741101")
+        assert "authorId" in author_json
+        assert "citationCount" in author_json
+        assert "paperCount" in author_json
+        assert "hIndex" in author_json
+        assert "affiliations" in author_json
+        assert isinstance(author_json["affiliations"], list)
 
 
 def test_get_author_data() -> None:
@@ -31,3 +36,22 @@ def test_get_author_data() -> None:
     # Check that id, citations, papercount, and h index are present in every author
 
     # Check that every author in the authorships list, has an author object
+
+
+def test_query_cache() -> None:
+    """Check that API queries are stored in the cache and persisted to disk"""
+    cache_path = ".api_cache_test"
+    test_url = "author/1741101?fields=affiliations,paperCount,citationCount,hIndex"
+
+    # Check queries are written to the in memory cache
+    with SemanticScholarQuerier(cache_path=cache_path) as query_engine:
+        query_engine._SemanticScholarQuerier__get_json(test_url) # type: ignore # mypy doesn't understand private variable accessing
+        assert test_url in query_engine._SemanticScholarQuerier__cache, "URL is not cached to dict" # type: ignore
+
+    # Open the persisted cache and check its contents
+    assert os.path.exists(cache_path), "Cache file not created"
+    with open(cache_path, "rb") as file:
+        cache = pickle.load(file)
+    assert test_url in cache, "Queried URL is not stored in cache"
+
+    os.remove(cache_path)

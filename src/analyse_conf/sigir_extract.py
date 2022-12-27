1ea7cd37d4d098207508ffa06c4349570dd96922
Liam Watts <liam.watts271@gmail.com>
2022-12-28 00:18:02 +1100

    sigir_extract: fix linting errors, improve docs

--- src/analyse_conf/sigir_extract.py
+++ src/analyse_conf/sigir_extract.py
@@ -9,30 +9,33 @@ from analyse_conf.data import Authorship, Paper
 
 
 def get_paper_tags(url: str) -> list[bs4.element.Tag]:
-    """Retrieve all the paragraph tags on the SIGIR page"""
+    """
+    Retrieve all the paragraph tags on the SIGIR page
+    These paragraph tags are papers, or contain a href and indicate the type of paper
+    """
     response = requests.get(url)
     response.raise_for_status()
     html = response.content
     soup = bs4.BeautifulSoup(html, "html.parser")
     body_text = soup.find("div", class_="post-body") # the main body of text
-    paragraphs = body_text.find_all("p") # All paragraph tags are papers, or contain a href and indicate the type of paper
+    paragraphs = body_text.find_all("p")
     return paragraphs
 
 
 def split_authors(author_str: str) -> list[str]:
     """Split the typical academic list of authors into a list of strings"""
     authors = author_str.strip().split(" and ")
-    split_authors = authors[0].split(", ")
+    separated_authors = authors[0].split(", ")
 
     # there are a few lone authors, avoid throwing an error for them
     # also some final authors are separated by a ", " not an " and ", in which case they're already handled
     if len(authors) > 1:
-        split_authors = split_authors + [authors[1]]
-    return split_authors
+        separated_authors.append(authors[1])
+    return separated_authors
 
 
 def extract_paper_data(paper_tags: list[bs4.element.Tag]) -> list[Paper]:
-    """Extract authorship, title and type information from the paper tags"""
+    """Extract authorship, title and type information from the paper tags into a list of Paper objects"""
     papers = []
     current_paper_type: str
     for tag in paper_tags:
@@ -53,5 +56,6 @@ def extract_paper_data(paper_tags: list[bs4.element.Tag]) -> list[Paper]:
 
 def extract_data() -> list[Paper]:
     """Return the paper and authorship data for SIGIR2022"""
-    paper_tags = get_paper_tags("https://sigir.org/sigir2022/program/accepted/") # Get all html elements that represent a paper
-    return extract_paper_data(paper_tags) # Extract and Store papers as a Paper object, with title, authors and type
+    paper_tags = get_paper_tags("https://sigir.org/sigir2022/program/accepted/")
+    papers = extract_paper_data(paper_tags)
+    return papers
